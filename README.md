a154d73d07cd0a099d5c9e78f9f91c9c2e505fe2
clemlak <clementlakhal@gmail.com>
2023-06-21 10:35:29 +0400

    🚀 we cooking

---
+++ README.md
@@ -0,0 +1,8 @@
+# Nugu
+
+Nugu is a [CREATE3](https://github.com/ZeframLou/create3-factory) factory contract deployed at the address `0x0000a79c3d8124ed3a7f8ec8427e4dc43a2b154d` on several EVM-compatible chains. Here are the current supported networks:
+
+- Ethereum Sepolia
+- Polygon Mumbai
+- Optimistic Goerli
+- Arbitrum Goerli
---
+++ script/NuguFactory.s.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import "../src/NuguFactory.sol";
+
+contract NuguFactoryScript is Script {
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        address factory = address(new NuguFactory());
+        console.log("Factory:", factory);
+
+        vm.stopBroadcast();
+    }
+}
