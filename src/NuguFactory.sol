f7938da16f6ed5b05e39bcb31e795fcaec842163
clemlak <clementlakhal@gmail.com>
2023-08-23 15:07:00 +0400

    feat: remove value param (use msg.value instead)

--- src/NuguFactory.sol
+++ src/NuguFactory.sol
@@ -4,13 +4,9 @@ pragma solidity 0.8.19;
 import "solmate/utils/CREATE3.sol";
 
 contract NuguFactory {
-    function deploy(bytes32 salt, bytes memory creationCode, uint256 value)
-        external
-        payable
-        returns (address deployed)
-    {
+    function deploy(bytes32 salt, bytes memory creationCode) external payable returns (address deployed) {
         salt = keccak256(abi.encodePacked(msg.sender, salt));
-        deployed = CREATE3.deploy(salt, creationCode, value);
+        deployed = CREATE3.deploy(salt, creationCode, msg.value);
     }
 
     function getDeployed(address deployer, bytes32 salt) external view returns (address) {
